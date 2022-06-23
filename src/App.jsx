905badde228be191d18ab8be621ce3359b029ee3
Anntrick <anntrick91@gmail.com>
2022-06-23 13:58:12 +0200

    ejercicio

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './containers/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Tiempo from './containers/Tiempo/Tiempo'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/tiempo" element={<Tiempo/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+const Footer = props => {
+    return (
+        <div className="Footer">
+            En caso de emergencias, llama a PepaPig
+        </div>
+    )
+}
+
+export default Footer 
