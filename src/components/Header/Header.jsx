ef5944e1f458f59ac34f24aa2a77cf0d6f467eb7
Anntrick <anntrick91@gmail.com>
2022-06-29 14:37:31 +0200

    con material ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom"
+import { AppBar, Container, Toolbar, Box, Button } from '@mui/material'
 import "./Header.scss"
 
 const Header = props => {
     return (
         <div className="Header">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/tiempo">El tiempo</NavLink>
+            <AppBar component="nav">
+                <Toolbar>
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        <Button as={NavLink} to="/" sx={{ color: '#fff' }}>
+                            Home
+                        </Button>
+                        <Button as={NavLink} to="/tiempo" sx={{ color: '#fff' }}>
+                            El tiempo
+                        </Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
--- src/containers/Tiempo/Tiempo.jsx
+++ src/containers/Tiempo/Tiempo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
+import { Card, CardContent, Typography, Grid } from '@mui/material'
 import WeatherCard from '../../components/WeatherCard/WeatherCard'
 
 import './Tiempo.scss'
@@ -7,21 +8,39 @@ import './Tiempo.scss'
 const Tiempo = props => {
     let [weather, setWeather] = useState([])
 
-    useEffect(()=> {
+    useEffect(() => {
         axios.get('https://www.el-tiempo.net/api/json/v2/home')
-        .then(resp => {
-            setWeather(resp.data.ciudades)
-        })
+            .then(resp => {
+                setWeather(resp.data.ciudades)
+            })
     }, [])
 
     return (
-        <div className="Tiempo">
+        <Grid container spacing={2} className="Tiempo">
             {
-                weather.map((city)=> (
-                    <WeatherCard data={city}/>
+                weather.map((city) => (
+                    <Grid item xs={12} md={6} lg={4}>
+                        <Card  >
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {city.name}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {city.stateSky.description}
+                                </Typography>
+                                <Typography gutterBottom variant="h6" component="div">
+                                    Temperatura
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    max {city.temperatures.max}
+                                    min {city.temperatures.min}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 ))
             }
-        </div>
+        </Grid>
     )
 }
 
